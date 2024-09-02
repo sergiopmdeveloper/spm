b4c11540dfdd1ca73010402ec20882a33352d915
Sergio <sergio.pm.developer@gmail.com>
2024-09-02 23:24:20 +0000

    ops: install and implement tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+});
